a898665ef61ed825c92714e3b9173be9f38c8650
Brian Wachanga <brian@talicraft.com>
2015-08-04 15:07:49 +0300

    Data pre-processing - convert recordedDate column to DateTime, Aggregate records in 15 minutes intervals, plotting aggregated data

--- data_collection.R
+++ data_collection.R
@@ -1,7 +1,10 @@
 library(rjson)
+library(lubridate)
+library(zoo)
+library(timeSeries)
 
 #REST endpoint for nairobi's traffic
-url <-"http://nairobitraffic-server.elasticbeanstalk.com/api/trafficRecord/?section=nhif-ngong-east"
+url <-"http://nairobitraffic-server.elasticbeanstalk.com/api/trafficRecord/?section=kenyatta-ave-west-into-rndbt&limit=1500"
 
 #JSON response from rest endpoint
 response <- scan(url, "", sep="\n")
@@ -16,9 +19,28 @@ results <- responseList$results
 trafficData <- do.call(rbind, lapply(results, function(input){
   #Convert a traffic object to a row
   row <- as.data.frame(input)
+  row$sectionName <- as.character(row$sectionName)
+  row$recordedDate <- as.character(row$recordedDate)
   return(row)
 }))
 
+#remove id and section name columns
+trafficData <- trafficData[,c(-1,-2)]
+
+#convert recorded date column to DateTime
+trafficData$recordedDate <- ymd_hms(trafficData$recordedDate, tz="GMT")
+
+#create time series object
+ordered <- zoo(trafficData$trafficRate , order.by = trafficData$recordedDate)
+
+#create regular intervals of 15 minutes
+regular <- aggregate(ordered, time(ordered) - as.numeric(time(ordered)) %% 900, mean)
+
+#convert to regular timeseries
+regular <- as.timeSeries(regular)
+
+plot(regular)
+
 #update the column names
-names(trafficData) <- c("ID", "Section Name", "Traffic Rate", "Timestamp")
+names(trafficData) <- c("Traffic Rate", "Timestamp")
 
